ab51774531dbdc20976fcf1559c402b00e2b73e6
marags-web <maragatham.muthusamy@intel.com>
2022-09-22 15:22:13 +0530

    Update ids.py

--- IDS/ids.py
+++ IDS/ids.py
@@ -80,7 +80,7 @@ le = LabelEncoder()
 #    socket.send_pyobj({"attack"})
 
 import socket
-HOST = socket.gethostbyname('ids_dns4')# Standard loopback interface address (localhost)
+HOST = socket.gethostbyname('ids-demo')# Standard loopback interface address (localhost)
 PORT = 9898        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
