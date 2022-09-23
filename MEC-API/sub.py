6aa9f123343788ef5eba1d901babbd39be8ff46a
marags-web <maragatham.muthusamy@intel.com>
2022-09-24 00:14:01 +0530

    Update sub.py

--- MEC-API/sub.py
+++ MEC-API/sub.py
@@ -1,25 +1,15 @@
-#import zmq
 
-#context = zmq.Context()
-#socket = context.socket(zmq.SUB)
-#socket.bind('tcp://mecapi-demo:8080') for IDS client 
-#socket.setsockopt_string(zmq.SUBSCRIBE,'')
-
-#while(True):
-#   message = socket.recv_pyobj()
-#   print(message)
-#socket.close()
 
 import socket
 HOST = 'localhost' 
 PORT = 2000        # Port to listen on (non-privileged ports are > 1023)
 
-#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#    s.connect((HOST,PORT))
-#    data = s.recv(1024)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST,PORT))
+    data = s.recv(1024)
 
 while True:
-   # print('Received the message', repr(data))
-    print ('Received from IDS : Attack    ')
+   print('Received from IDS : ', repr(data))
+   
 
 
