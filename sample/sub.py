5770fc7662732811423a9f4c6e4d72b9f2e06308
marags-web <maragatham.muthusamy@intel.com>
2022-09-23 20:01:59 +0530

    Create sub.py

---
+++ sample/sub.py
@@ -0,0 +1,10 @@
+import socket
+HOST = 'localhost' 
+PORT = 2000        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST,PORT))
+    data = s.recv(1024)
+
+while True:
+    print('Received', repr(data))
