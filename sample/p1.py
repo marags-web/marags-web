5d912c2bedf38d60222cb30fc6736fbe7122c7f6
marags-web <maragatham.muthusamy@intel.com>
2022-09-23 18:48:38 +0530

    Create p1.py

---
+++ sample/p1.py
@@ -0,0 +1,20 @@
+import socket
+#HOST = socket.gethostbyname('ids_dns4')# Standard loopback interface address (localhost)
+#PORT = 9898        # Port to listen on (non-privileged ports are > 1023)
+HOST = 'mec2'
+PORT = 2000 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #s.connect((HOST,PORT))
+    s.bind((HOST,PORT))
+    s.listen()
+    conn, addr = s.accept()
+
+#   with conn:
+        #print('Connected by', addr)
+
+    while True:
+        data = (b'attack')
+        #if not data:
+        #   break
+        conn.sendall(data)
+
