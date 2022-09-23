41ca205b41942bc2d8b5667acaf0d372000ce47c
marags-web <maragatham.muthusamy@intel.com>
2022-09-23 23:27:13 +0530

    Update p1.py

--- sample/p1.py
+++ sample/p1.py
@@ -8,6 +8,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST,PORT))
     s.listen()
     conn, addr = s.accept()
+    print("Inferencing the traffic")
 
 #   with conn:
         #print('Connected by', addr)
