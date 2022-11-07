3610435520bf2d0bb4ce62004c9be9b629e2c548
marags-web <maragatham.muthusamy@intel.com>
2022-11-07 20:58:51 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -6,6 +6,12 @@ def main():
     """Say hello"""
     return 'Hello, world!'
 
+@app.route('/',methods=['GET','POST'])
+
+def home():
+     if(request.method == 'GET'):        
+            return 'I Am in get method'
+
 if __name__ == '__main__':  # Script executed directly?
     print("Hello World! Built with a Docker file.")
     app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True) 
