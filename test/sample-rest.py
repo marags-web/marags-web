a0d211784c3b9c52083c61e7d2ca93ff36e5dd5b
marags-web <maragatham.muthusamy@intel.com>
2022-11-07 14:25:59 +0530

    Update sample-rest.py

--- test/sample-rest.py
+++ test/sample-rest.py
@@ -1,14 +1,49 @@
 from flask import Flask,jsonify,request
+import json 
+
 app = Flask(__name__)
+
 @app.route('/',methods=['GET','POST'])
+
 def home():
+ 
     if(request.method == 'GET'):
-       data = "hello"
-       index =1
-       return jsonify({'data':data},{index:index})
-       #return jsonify({'data':data})
+        f= open ('subscribe.json','r')
+        data = json.load(f)
+        for i in data["subscription_details"]:
+            return jsonify(data)
+        f.close()
+    
     if(request.method == 'POST'):
-       return jsonify({'data':'this is a test'})
-   
+        name = request.json['user_name'] 
+        email = request.json['email']
+        sub_type = request.json['sub_type']
+        
+        row1 = {'user_name':str(name),'email':str(email),'sub_type':str(sub_type)}
+        print(row1)
+       
+        # Writing to a file - getting issues since some extra char is added at the end 
+        #str1 = json.dumps(row1)
+        f = open ('subscribe.json','r')
+        data = json.loads(f.read())
+        f.close()
+        data["subscription_details"].append(row1)
+        f = open ('subscribe.json','w')
+        f.write(json.dumps(data))
+        f.close()
+        #f= open ('subscribe.json','a')
+        #data = json.loads(f)
+        #data["subscription_details"].append(row1)
+        #f.write(data)
+        #f.close()
+        return jsonify(row1)
+
+    if(request.method == 'DELETE'):
+       print ("Deleted")
+    
+    if(request.method == 'PUT'):
+       print ("Updated")
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
+
