28b1d27316585d6b9f0008b73559b30846c67b90
marags-web <maragatham.muthusamy@intel.com>
2022-11-07 21:36:01 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,13 +1,12 @@
 from flask import Flask  # From module flask import class Flask
 app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
-@app.route('/')   # URL '/' to be handled by main() route handler
-def main():
-    """Say hello"""
-    return 'Hello, world!'
+#@app.route('/')   # URL '/' to be handled by main() route handler
+#def main():
+#    """Say hello"""
+#    return 'Hello, world!'
 
 @app.route('/',methods=['GET','POST'])
-
 def home():
      if(request.method == 'GET'):        
             return 'I Am in get method'
