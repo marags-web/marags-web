fbc43451fa50ad5a22cb553ec7910f60c0f98b2d
marags-web <maragatham.muthusamy@intel.com>
2022-11-07 14:24:55 +0530

    Update httpreq.py

--- http/httpreq.py
+++ http/httpreq.py
@@ -1,7 +1,23 @@
 import requests
+import json
+url ='http://127.0.0.1:5000/'
 
-url ='http://127.0.0.1:5000'
-myobj = {'data':'hi'}
-
-x = requests.post(url,json=myobj)
+x= requests.get(url)
 print(x.text)
+
+
+myobj = {"user_name":"Diyo","email" :"diyo@gmail.com", "sub_type" : "Probing"}
+
+if( myobj['sub_type'] != 'normal'):
+    response = requests.post(url,json=myobj)
+
+myobj = {"user_name":"Eal","email" :"eal@gmail.com", "sub_type" : "normal"}
+
+if( myobj['sub_type'] != 'normal'):
+   response = requests.post(url,json=myobj)
+
+myobj = {"user_name":"Xian","email" :"xian@gmail.com", "sub_type" : "DoS"}
+
+if( myobj['sub_type'] != 'normal'):
+    response = requests.post(url,json=myobj)
+
